b2e44d4d19873d4e97c571d3eb8fd4647553d846
Tony Storti <tonystorti@gmail.com>
2017-05-11 16:40:21 -0500

    created file structure, added notes for accessing API data

---
+++ assets/javascript/logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   
+   	var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+	//to access data, query below URL with the channel name for JSON data
+	//see also https://wind-bow.glitch.me/ for more info
+	
+	var url='https://wind-bow.glitch.me/twitch-api/channels/' 
+
+	$.getJSON(url, function(json) {
+  		
+	});
+  
+
+
+
