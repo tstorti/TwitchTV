a6f9e43f0fdd3c28ccb904f880ea8ed714821623
Tony Storti <tonystorti@gmail.com>
2017-05-17 16:23:55 -0500

    configured getJSON for API

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,15 +1,45 @@
 $(document).ready(function() {
    
-   	var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-	//to access data, query below URL with the channel name for JSON data
-	//see also https://wind-bow.glitch.me/ for more info
-	
-	var url='https://wind-bow.glitch.me/twitch-api/channels/' 
 
-	$.getJSON(url, function(json) {
-  		
-	});
+   	var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	//to access data, query below URL with the channel name for JSON data
+	
+	//for all of the channels in the array, fetch JSON data and put data in the twitchData array.
+	
+	for(var i=0;i<channels.length;i++){
+		var url='https://wind-bow.glitch.me/twitch-api/channels/'+channels[i]; 
+		$.getJSON(url, function(json) {
+	  		
+	  		$("#all").append('<div>'+json.display_name+'</div>');
+
+	  		//if user is offline, status will be null
+	  		if(json.status===null){
+	  			$("#offline").append('<div>'+json.display_name+'</div>');
+	  		}
+	  		else{
+	  			$("#online").append('<div>'+json.display_name+'</div>');
+	  			//create a link with the user's logo to take them to the twitch page
+	  			$("#online").append('<a href='+json.url+'><image class="logo" src="'+json.logo+'"></a>');
+
+	  		}
+
+
+
+
+
+
+
+
+
+		});
   
+	}
+	
+
+});
+
+
+	
 
 
 
